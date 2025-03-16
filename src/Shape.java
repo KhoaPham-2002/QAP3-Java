f43510ef22be17d1273c2499f9cac0bf60702bc4
Khoa Pham <Khoa.Pham@keyin.com>
2025-03-16 12:03:45 -0400

    Updated Shape class, all of the requirements have been completed

---
+++ src/Shape.java
@@ -0,0 +1,22 @@
+// Name: Khoa Pham
+// Project: QAP3 Java
+// Date: 03/16/2025
+
+public abstract class Shape {
+    protected String name;
+
+    // Constructor
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    // Abstract methods for area and perimeter
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    // toString() method to display shape information
+    @Override
+    public String toString() {
+        return String.format("Shape: %s%nArea: %.2f%nPerimeter: %.2f%n", name, getArea(), getPerimeter());
+    }
+}
