8f13d2fda1f3eea60faeb5443385e0b5ebd4b0cb
Khoa Pham <Khoa.Pham@keyin.com>
2025-03-16 12:23:33 -0400

    Updated Demo3, all of the tesings for the Circle, Ellipse, Triangle, EquilateralTriangle have been completed

---
+++ src/Demo3.java
@@ -0,0 +1,25 @@
+// Name: Khoa Pham
+// Project: QAP3 Java
+// Date: 03/16/2025
+
+public class Demo3 {
+    public static void main(String[] args) {
+        // Create an array of Shape references
+        Shape[] shapes = new Shape[5];
+
+        // Initialize different shapes
+        shapes[0] = new Circle(5.0);
+        shapes[1] = new Ellipse(6.0, 4.0);
+        shapes[2] = new Triangle(3.0, 4.0, 5.0);
+        shapes[3] = new EquilateralTriangle(4.0);
+        shapes[4] = new Ellipse(7.0, 3.0);
+
+        // Print details of each shape using a traditional for loop
+        System.out.println("=== Shape Details ===");
+        for (int i = 0; i < shapes.length; i++) {
+            System.out.println(shapes[i]);
+            System.out.println("--------------------------------");
+        }
+    }
+}
+
